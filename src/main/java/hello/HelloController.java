78fdbd6bec5e183d1523244879eb029d4607215c
TalGur <tal.goor@gmail.com>
2018-02-06 17:22:04 +0200

    adding /greeting requestmapping

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +10,11 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Greetings "+name+"from Spring Boot!";
     }
     
 }
