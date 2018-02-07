26711f5a4d02016b90bf871ecb04214aa9137ad7
TalGur <tal.goor@gmail.com>
2018-02-07 11:44:30 +0200

    cleanup

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -5,37 +5,33 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.Console;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class HelloController {
 
-    Map<Long, Greeting> greetings ;
+    private Map<Long, Greeting> greetings ;
 
     public HelloController() {
-        this.greetings  = new HashMap<Long, Greeting>();
+        greetings  = new HashMap<>();
         greetings.put((long) 1, new Greeting(1,"Greetings from Spring Boot 111!"));
         greetings.put((long) 2, new Greeting(2,"Greetings from Spring Boot 222!"));
     }
 
     @RequestMapping("/")
     public String index() {
-        Greeting greeting = greetings.get((long) 1);
-        return greeting.toString();
+        return greetings.get((long) 1).toString();
     }
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        Greeting greeting = greetings.get((long) 2);
-        return greeting;
+        return greetings.get((long) 2);
     }
 
     @RequestMapping(value="/greeting/{id}")
     public Greeting greeting(@PathVariable("id") long id) {
-        Greeting greeting = greetings.get(id);
-        return greeting;
+        return greetings.get(id);
     }
     
 }
